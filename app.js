dc588fdf924b2100574400fb5b7a18573b44beb5
masud rana <masudranaacb@gmail.com>
2022-03-08 22:21:52 -0800

    for loop throw in array

--- app.js
+++ app.js
@@ -15,11 +15,17 @@ else{
 }
 // multiple condition &&,||
 if(number===4 && name==='rakib'){
-    console.log('boat a maching')
+    // console.log('boat a maching')
 }
 else if(number===3 || name==='masud'){
-    console.log('just name is maching')
+    // console.log('just name is maching')
 }
 else{
-    console.log('not a mCHING')
+    // console.log('not a mCHING')
+}
+// 3 Array declare
+// Array index,length,push
+const numbers=[25,45,7,56,8]
+for(let i=0; i<numbers.length; i++){
+    console.log(numbers[i])
 }
