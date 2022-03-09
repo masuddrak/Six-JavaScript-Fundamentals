5c11f5e3b184c6f6a1845f914b65c2aa55bfc190
masud rana <masudranaacb@gmail.com>
2022-03-08 22:07:35 -0800

    using a simple condition

---
+++ app.js
@@ -0,0 +1,17 @@
+//1 how to Declaret a veriable const & let 
+const fatherName="Abul Kalam Azad"
+let color='red'
+color="green"
+
+// 2 basic 6 condition ===, !==, <, >, =<, >=
+const number=4
+const name='masud'
+
+if(number===5){
+    // console.log('this number 5')
+}
+else{
+    // console.log('number is not equal 5')
+}
+// multiple condition &&,||
+
